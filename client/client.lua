0db875a1a8b0989edbca75388cd5ad579a4bbfc5
MOVZX <movzx@yahoo.com>
2023-01-31 21:09:38 +0700

    Disable player chocking each other when PVP mode is OFF
    - Even though PvP mode is disabled, we can still choke other players,
      let's fix that issue.
    
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- client/client.lua
+++ client/client.lua
@@ -3,17 +3,24 @@ local RSGCore = exports['rsg-core']:GetCoreObject()
 local pvp = true
 
 RegisterNetEvent('rsg-pvp:client:pvpToggle',function()
+    local ped = PlayerPedId()
 
     if pvp == true then
         SetRelationshipBetweenGroups(1,`PLAYER`, `PLAYER`)
         NetworkSetFriendlyFireOption(false)
+        Citizen.InvokeNative(0xB8DE69D9473B7593, ped, 6) -- Disable choking other people
+
         pvp = false
+
         RSGCore.Functions.Notify(Lang:t('primary.pvp_off'), 'primary')
-    else
-        SetRelationshipBetweenGroups(5, `PLAYER`, `PLAYER`)
-        NetworkSetFriendlyFireOption(true)
-        pvp = true
-        RSGCore.Functions.Notify(Lang:t('primary.pvp_on'), 'primary')
+        return
     end
 
+    SetRelationshipBetweenGroups(5, `PLAYER`, `PLAYER`)
+    NetworkSetFriendlyFireOption(true)
+    Citizen.InvokeNative(0x949B2F9ED2917F5D, ped, 6) -- Enable choking other people
+
+    pvp = true
+
+    RSGCore.Functions.Notify(Lang:t('primary.pvp_on'), 'primary')
 end)
